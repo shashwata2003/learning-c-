b12673665ba69c919646c92ce11ab5078356785f
shashwata <shashwatasamanta5@gmail.com>
2021-07-14 12:14:51 +0530

    added

--- Armyage.cpp
+++ Armyage.cpp
@@ -9,9 +9,19 @@ int main(){
     cin>>age;
     c=age-26;
     cout<<"Your age after reducing the service in army is = "<<c<<endl;
-    if(c<=18){
+    if(c==18){
         cout<<"You are allowed to fill the form "<<endl;
     }
+    else if ( c>34 ){
+        cout<<"You are overaged to fill the form. "<<endl;
+    }
+    else if (c<18){
+        cout<<"You are underaged to fill the  form."<<endl;
+    }
+    else {
+        cout<<"You are allowed to fill the form. "<<endl;
+    }
+
 
 
 return 0;

62b917ddb211fec0a3190800f2b71e03349df6a2
shashwata <shashwatasamanta5@gmail.com>
2021-07-16 08:21:07 +0530

    added

--- shops.cpp
+++ shops.cpp
@@ -1,50 +1,41 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-class Shops
+class shops
 {
 private:
     int itemid[100];
     int rownumber[100];
-    int itemcost[100];
+    int price[100];
     int counter;
 
 public:
-    void intcounter(void) { counter = 0; }; 
-    void datainput(void);
-    void display(void);
-};
+    void intcounter(void){
+        counter;
+    }
 
-void Shops ::datainput(void)
-{ 
-    cout << "Enter the item ID of the product" <<counter+1<< endl;
-    cin >> itemid[counter];
-    cout << "Enter the row number in which this product belong = " << endl;
-    cin >> rownumber[counter];
-    cout << "Enter the item Cost of the product = " << endl;
-    cin >> itemcost[counter];
-};
-void Shops::display(void)
-{
-    for (int i = 0; i < counter; i++)
-    {
-        cout << "The price of the item with ID " << itemid[i] << " is " << itemcost[i] << "."
-             << " This product should be kept in the row " << rownumber[i] << endl;
+    void input(void){
+        cout<<"Enter the item id of the product = "<<counter + 1<<endl;
+        cin>>itemid[counter];
+        cout<<"Enter the row to which it belongs = "<<endl;
+        cin>>rownumber[counter];
+        cout<<"Enter the price of the item = "<<endl;
+        cin>>price[counter];
+        counter++;
+    }
+    void display(void){
+        for (int i = 0; i < counter; i++)
+        {
+         cout<<"The price of the item with the ID "<<itemid[i]<<" is "<<price[i]<<"."<<"It belongs to the row number "<<rownumber[i]<<endl;
+        }
     }
 };
 
-int main()
-{
-Shops Shop_1 ;
-Shop_1.intcounter();
-
-
-    Shop_1.datainput();
-    Shop_1.datainput();
-    Shop_1.datainput();
-    Shop_1.display();
-
-
-
-
-    return 0;
+int main(){
+    shops shop_1;
+    shop_1.intcounter();
+    shop_1.input();
+    shop_1.input();
+    shop_1.display();
+ 
+return 0;
 }

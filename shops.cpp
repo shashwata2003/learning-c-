0441c3558d885cf66f8201519c7d1d5a7e2990dd
shashwata <shashwatasamanta5@gmail.com>
2021-07-16 15:36:50 +0530

    added

--- shops.cpp
+++ shops.cpp
@@ -1,41 +1,46 @@
 #include<iostream>
 using namespace std;
+
 class shops
 {
 private:
     int itemid[100];
-    int rownumber[100];
     int price[100];
+    int rownumber[100];
     int counter;
 
 public:
-    void intcounter(void){
-        counter;
-    }
-
+     void intcounter(void){
+         counter=0;
+     }
     void input(void){
-        cout<<"Enter the item id of the product = "<<counter + 1<<endl;
+        cout<<"Enter the Item ID of the product "<<counter + 1<<" = "<<endl;
         cin>>itemid[counter];
-        cout<<"Enter the row to which it belongs = "<<endl;
-        cin>>rownumber[counter];
-        cout<<"Enter the price of the item = "<<endl;
+        cout<<"Enter the Price of the product = "<<endl;
         cin>>price[counter];
+        cout<<"Enter the row number of the product = "<<endl;
+        cin>>rownumber[counter];
         counter++;
-    }
+    };
     void display(void){
         for (int i = 0; i < counter; i++)
         {
-         cout<<"The price of the item with the ID "<<itemid[i]<<" is "<<price[i]<<"."<<"It belongs to the row number "<<rownumber[i]<<endl;
+            cout<<"The Price of the item with the itemID "<<itemid[i]<<" is "<<price[i]<<"."<<"It belongs to the row number "<<rownumber[i]<<endl;
         }
-    }
+        
+    };
 };
 
+
+
 int main(){
-    shops shop_1;
-    shop_1.intcounter();
-    shop_1.input();
-    shop_1.input();
-    shop_1.display();
- 
+    shops shopone;
+    shopone.intcounter();
+    shopone.input();
+    shopone.input();
+    shopone.input();
+    shopone.display();
+
+
 return 0;
 }

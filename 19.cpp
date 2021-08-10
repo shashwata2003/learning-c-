5ccda461bd284544921b6efdff2c56e47de87149
shashwata <shashwatasamanta5@gmail.com>
2021-08-10 19:19:44 +0530

    added

--- 19.cpp
+++ 19.cpp
@@ -1,6 +1,41 @@
 #include<iostream>
 using namespace std;
+
+class ShopItem
+{
+    int id;
+    float price;
+    public:
+        void setData(int a, float b){
+            id = a;
+            price = b;
+        }
+        void getData(void){
+            cout<<"Code of this item is "<< id<<endl;
+            cout<<"Price of this item is "<<price<<endl;
+        }
+};
 int main(){
+    int size,p;
+    float q;
+    cout<<"Enter the number of times you want run the program = "<<endl;
+    cin>>size;
+    ShopItem *ptr = new ShopItem[size];
+    ShopItem *ptrtemp = ptr;
+    for (int  i = 0; i < size; i++)
+    {
+        cout<<"Enter the price and the ID of the product "<<i+1<<" = "<<endl;
+        cin>>p>>q;
+        ptr ->setData(p,q);
+        ptr++;
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        ptrtemp ->getData();
+        ptrtemp++;
+    }
+    
 
 return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,2 @@
- // for (int i = 0; i < size; i++)
-    // {
-    //     ptrtemp++; 
-    // }
+
+int main(){

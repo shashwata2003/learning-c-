c63843e23e48120278c8ba329098cd2c8db35465
shashwata <shashwatasamanta5@gmail.com>
2021-08-16 11:27:39 +0530

    added

--- 20.cpp
+++ 20.cpp
@@ -24,19 +24,45 @@ class CWHvedio : public CWH{
         vedlenght = vl;
     }
     void display(){
-        cout<<"This the amazing vedio with the title = "<<title<<endl;
+        cout<<"This is amazing vedio with the title = "<<title<<endl;
         cout<<"The rating of this video is =  "<<rating<<endl;
         cout<<"The lenght of the vedio is = "<<vedlenght<<endl;
     }
 };
 
 class CHWtext : public CWH{
-    float words;
+    int  words;
     public:
-    CHWtext()
-}
+    CHWtext(string s, float r ,int wc ) : CWH(s , r){
+        words = wc;
+    }
+    void display(){
+        cout<<"This is a amazing text tutorial with the title = "<<title<<endl;
+        cout<<"The rating of the text tutorial is "<<rating<<endl;
+        cout<<"The number of words in this text is "<<words<<endl;
+    }
+}; 
 
 int main(){
+    string title;
+    float rating,vedlenght;
+    int words;
 
+    title = "vedio one";
+    rating=4.9;
+    vedlenght=16.56;
+    CWHvedio one(title,rating,vedlenght);
+
+    title="Vedio one notes";
+    rating=4.3;
+    words=450;
+    CHWtext two(title,rating,words);
+
+    CWH*tut[2];
+    tut[0]=&one;
+    tut[1]=&two;
+
+    tut[0]->display();
+    tut[1]->display(); 
 return 0;
 }

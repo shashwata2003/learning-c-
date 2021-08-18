3b217d3a06d2e89b54ed08c94725044df47b8314
shashwata <shashwatasamanta5@gmail.com>
2021-08-18 20:17:05 +0530

    added

--- 21.cpp
+++ 21.cpp
@@ -6,8 +6,9 @@ string st = "hello world";
 string st2;
 // ofstream out("text21.txt");
 // out<<st;
-ifstream in("text21.txt");
+ifstream in("text21b.txt");
 in>>st2;
 getline(in,st2);
+cout<<st2;
 return 0;
 }

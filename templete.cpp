e8c4bdffd0f64ecbf43cf8410e8778b98401e247
shashwata <shashwatasamanta5@gmail.com>
2021-08-20 11:11:41 +0530

    added

--- templete.cpp
+++ templete.cpp
@@ -8,14 +8,14 @@ public:
     int size;
     vector(int m)
     {
-        m = size;
+        size = m;
         arr = new T[size];
     }
     T dotproduct(vector &v){
         T d = 0;
         for (int i = 0; i < size; i++)
         {
-            d += this->arr[i]*v.arr[i];
+            d += this->arr[i] * v.arr[i];
         }
         return d;
     }
@@ -24,15 +24,17 @@ public:
 int main()
 {
     vector<float> v1(3);
-    v1.arr[0]=3.4;
-    v1.arr[1]=1.2;
-    v1.arr[2]=2.4;
+    v1.arr[0]=3;
+    v1.arr[1]=1;
+    v1.arr[2]=2;
 
     vector<float> v2(3);
-    v2.arr[0]=1.3;
-    v2.arr[1]=2.3;
-    v2.arr[2]=26.5;
-    float a = v1.dotproduct(v2);
+    v2.arr[0]=1;
+    v2.arr[1]=2;
+    v2.arr[2]=2;
+    
+    float a = v2.dotproduct(v1);
+    cout<<a<<endl;
 
     return 0;
 }
